9fd403a9f4baa031e24bfa30fac20367254025e0
Jution Candra Kirana <jutionck@gmail.com>
2022-08-25 09:27:48 +0700

    Hands On - Hero

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@ import { Hero } from './model/hero';
 
 const daud: Hero = new Hero('Daud', 100, 100, 10);
 const fahlan: Hero = new Hero('Fahlan', 100, 100, 20);
+const riza: Hero = new Hero('Riza', 100, 100, 10);
+const aisyah: Hero = new Hero('Aisyah', 100, 100, 10);
+const rasyid: Hero = new Hero('Rasyid', 100, 100, 40);
 
+console.log(fahlan.getName(), 'current hp :', fahlan.getHp());
+console.log(daud.getName(), 'attack', fahlan.getName());
 daud.attack(fahlan);
-console.log('Fahlan:', fahlan);
-console.log('Daud', daud);
+console.log(fahlan.getName(), 'current hp :', fahlan.getHp());
+
+console.log(riza.getName(), 'current mana :', riza.getMana());
+console.log(riza.getName(), 'heal', fahlan.getName());
+riza.heal(fahlan);
+console.log(riza.getName(), 'current mana :', riza.getMana());
+console.log(fahlan.getName(), 'current hp :', fahlan.getHp());
+console.log(riza.getName(), 'current hp :', riza.getHp());
+
+
--- src/model/hero.ts
+++ src/model/hero.ts
@@ -4,27 +4,49 @@ export class Hero {
   private mana: number;
   private baseDamage: number;
 
-  constructor(
-      name: string,
-      hp: number,
-      mana: number,
-      baseDamage: number
-  ) {
+
+  constructor(name: string, hp: number, mana: number, baseDamage: number) {
     this.name = name;
     this.hp = hp;
     this.mana = mana;
     this.baseDamage = baseDamage;
   }
 
-  getHit(damage: number): void {
-    this.hp -= damage;
-  }
-
-  receiveDamage(damage: number): void {
-    this.hp -= damage;
+  receiveDamage(hero: Hero): void {
+    this.hp -= hero.getBaseDamage();
   }
 
   attack(hero: Hero): void {
-    hero.receiveDamage(this.baseDamage)
+    hero.receiveDamage(this)
+  }
+
+  heal(hero: Hero): void {
+    hero.hp += this.mana/2;
+    this.mana -= this.mana/2;
+  }
+
+  getBaseDamage(): number {
+    return this.baseDamage
+  }
+
+  getName(): string {
+   return this.name;
+  }
+
+  getHp(): number {
+    return this.hp;
+  }
+
+  getMana(): number {
+    return this.mana
+  }
+
+  toString(): string {
+    return "Hero { " +
+        "name= " + this.name +
+        ", hp= " + this.hp +
+        ", mana= " + this.mana +
+        ", baseDamage= " + this.baseDamage +
+        " }";
   }
 }

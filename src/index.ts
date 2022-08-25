1caf923a22f9d1437442538573349a2363591122
Jution Candra Kirana <jutionck@gmail.com>
2022-08-25 10:08:23 +0700

    Response Template

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,27 @@
-// Generic
-// Flexible untuk menerima jenis tipe data
-// Keyword generic -> T
-// Penggunaan nya bisa di function, class, interface
+// Template Pagination & Single Response
 
-// Before
-function getData(items: any[]) : any[] {
-  return [].concat(items);
+interface Paging {
+  page: number;
+  rowsPerPage: number;
+  totalRows: number;
+  totalPages: number;
 }
 
-const people: any[] = getData(['John', 'Alex', 'Kohl']);
-const dadu: any[] = getData([1,2,3,4,5,6]);
-console.log(people);
-console.log(dadu);
-
-function getDataOther<T>(items: T[]) : T[] {
-  return new Array<T>().concat(items);
+interface ResponseEntity<T> {
+  code: number;
+  message: string;
+  data: T
 }
 
-const people02: Array<string> = getDataOther<string>(['John', 'Alex', 'Kohl']);
-const dadu02: Array<number> = getDataOther<number>([1,2,3,4,5,6]);
-console.log(people02);
-console.log(dadu02);
-
-type Employee = {
-  name: string;
-  age: number;
-  skills?: string[];
+class SingleResponse<T> implements ResponseEntity<T> {
+  code: number;
+  message: string;
+  data: T;
 }
 
-const employees: Employee[] = getDataOther<Employee>([
-    {name: 'Daud', age: 20},
-    {name: 'Fahlan', age: 20},
-    {name: 'Riza', age: 20},
-    {name: 'Aisyah', age: 20},
-]);
-console.log(employees);
+class PagedResponse<T> implements ResponseEntity<T> {
+  code: number;
+  message: string;
+  data: T;
+  paging: Paging
+}

b828616f44cb3a8d18d116608c23aac0a9ba21c0
Jution Candra Kirana <jutionck@gmail.com>
2022-08-26 10:43:49 +0700

    Hands On - Kulkas Observable 2

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,58 @@
 import { Kulkas } from './observable/kulkas';
+import { catchError, of, switchMap, tap } from 'rxjs';
 
 const kulkas: Kulkas = new Kulkas();
-kulkas.buka().subscribe((data) => console.log(data));
-kulkas.action.lihat().subscribe((data) => console.log(data));
-kulkas.action.simpan('Gajah').subscribe((data) => console.log(data));
-kulkas.action.simpan('Nanas').subscribe((data) => console.log(data));
-kulkas.action.lihat().subscribe((data) => console.log(data));
-kulkas.action.ambil('Gajah').subscribe((data) => console.log(data));
-kulkas.action.ambil('Gajah').subscribe((data) => console.log(data));
-kulkas.action.lihat().subscribe((data) => console.log(data));
-setTimeout(() => {
-  kulkas.tutup().subscribe((data) => console.log(data))
-}, 3000)
+const action = kulkas.buka();
+action.pipe(
+    tap((data) => console.log('tap.action.lihat: ', data)),
+    switchMap(() => {
+      return kulkas.action.lihat();
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.simpan: ', data)),
+
+    switchMap(() => {
+      return kulkas.action.simpan('Gajah');
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.simpan: ', data)),
+
+    switchMap(() => {
+      return kulkas.action.simpan('Nanas');
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.lihat: ', data)),
+    switchMap(() => {
+      return kulkas.action.lihat();
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.ambil: ', data)),
+    switchMap(() => {
+      return kulkas.action.ambil('Gajah');
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.ambil: ', data)),
+    switchMap(() => {
+      return kulkas.action.ambil('Gajah');
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    }),
+    tap((data) => console.log('tap.action.tutup: ', data)),
+    switchMap(() => {
+      return kulkas.tutup();
+    }),
+    catchError((error) => {
+      return of('Ada error')
+    })
+).subscribe((data) => console.log('subscribe:',data))
